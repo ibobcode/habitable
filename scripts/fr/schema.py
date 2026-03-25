cf79b08d7904388f23708903898026ad41e68d6f
Dylan HEIRSTRAETEN <dylan.heirstraeten@epitech.eu>
2026-03-25 14:12:10 -0300

    Create schema.py

---
+++ scripts/fr/schema.py
@@ -0,0 +1,22 @@
+# Single source of truth for all data fields # Add new fields here before creating a new data script
+
+SCHEMA = {
+    'h3_index': str,
+    'lat': float,
+    'lon': float,
+    'nuclear_plant_distance_km': float,
+    'rainfall_winter': float,
+    'rainfall_spring': float,
+    'rainfall_summer': float,
+    'rainfall_autumn': float,
+    'temperature_winter': float,
+    'temperature_spring': float,
+    'temperature_summer': float,
+    'temperature_autumn': float,
+    'temp_min': float,
+    'temp_max': float,
+    'sunshine_hours': float,
+    'rainy_days': float,
+    'frost_days': float,
+    'hot_days': float,
+}
